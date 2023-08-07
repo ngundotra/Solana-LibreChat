03407c908cdf9dcc862f037a6d2bea459ccd8fe5
ngundotra <noah@gundotra.org>
2023-08-07 15:02:53 -0400

    add sidekick branding

--- client/src/components/Input/Footer.tsx
+++ client/src/components/Input/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
         rel="noreferrer"
         className="underline"
       >
-        {config?.appTitle || 'LibreChat'}
+        PENIS {config?.appTitle || 'LibreChat'}
       </a>
       . Serves and searches all conversations reliably. All AI convos under one house. Pay per call
       and not per month (cents compared to dollars).
