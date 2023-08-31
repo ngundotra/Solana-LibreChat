38ebc268e501434c61b965c164b2db4955d112c7
ngundotra <noah@gundotra.org>
2023-08-31 16:25:35 -0400

    show torn down UI for mobile

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -13,11 +13,32 @@ import { Outlet } from 'react-router-dom';
 import store from '~/store';
 import { useAuthContext } from '~/hooks/AuthContext';
 import { useSetRecoilState } from 'recoil';
+import { useMediaQuery } from 'react-responsive';
+
+function GiphyEmbed() {
+  return (
+    <div>
+      <div style={{ width: '100%', height: '0', paddingBottom: '100%', position: 'relative' }}>
+        <iframe
+          src="https://giphy.com/embed/93YOJpS4wnMfycnslY"
+          width="100%"
+          height="100%"
+          style={{ position: 'absolute' }}
+          frameBorder="0"
+          className="giphy-embed"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
 
 export default function Root() {
   const setIsSearchEnabled = useSetRecoilState(store.isSearchEnabled);
   const setEndpointsConfig = useSetRecoilState(store.endpointsConfig);
   const setPresets = useSetRecoilState(store.presets);
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   const { user, isAuthenticated } = useAuthContext();
 
   const [navVisible, setNavVisible] = useState(() => {
@@ -71,8 +92,20 @@ export default function Root() {
         <Nav navVisible={navVisible} setNavVisible={setNavVisible} />
         <div className="flex h-full w-full flex-1 flex-col bg-gray-50">
           <div className="transition-width relative flex h-full w-full flex-1 flex-col items-stretch overflow-hidden bg-white pt-10 dark:bg-gray-800 md:pt-0">
-            <MobileNav setNavVisible={setNavVisible} />
-            <Outlet />
+            {isMobile ? (
+              <div className="place-self-center text-white">
+                <h2 className="text-5xl">Sidekick</h2>
+                <span>
+                  <p>Coming soon to mobile</p>
+                </span>
+                <GiphyEmbed />
+              </div>
+            ) : (
+              <>
+                <MobileNav setNavVisible={setNavVisible} />
+                <Outlet />
+              </>
+            )}
           </div>
         </div>
       </div>
