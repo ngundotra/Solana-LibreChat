f7d0620a9ca4ec7fd75f291bd4ef208f67c6c748
ngundotra <noah@gundotra.org>
2023-09-22 11:46:52 -0400

    Add Privacy Policy, update TOS link

--- client/src/components/Input/Footer.tsx
+++ client/src/components/Input/Footer.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
-import { useGetStartupConfig } from '@librechat/data-provider';
+// import { useGetStartupConfig } from '@librechat/data-provider';
 
 export default function Footer() {
-  const { data: config } = useGetStartupConfig();
+  // const { data: config } = useGetStartupConfig();
   return (
     <div className="hidden px-3 pb-1 pt-2 text-center text-xs text-black/50 dark:text-white/50 md:block md:px-4 md:pb-4 md:pt-3">
       <a
-        href="https://github.com/ngundotra/Solana-LibreChat/tree/sgpt"
+        href="https://chatgpt.solanalabs.com"
         target="_blank"
         rel="noreferrer"
         className="underline"
       >
-        {'Github'}
+        {'See more information'}
       </a>
       {'. By using this website you agree to the following '}
       {/* . Serves and searches all conversations reliably. All AI convos under one house. Pay per call
       and not per month (cents compared to dollars). */}
       <a
-        href="https://solanatechservices.com/tos"
+        href="https://solanatechservices.com/tos.pdf"
         target="_blank"
         rel="noreferrer"
         className="underline"
       >
-        {'Terms of Service.'}
+        {'Terms of Service'}
+      </a>
+      {' and '}
+      <a
+        href="https://solanatechservices.com/privacy.pdf"
+        target="_blank"
+        rel="noreferrer"
+        className="underline"
+      >
+        {'Privacy Policy.'}
       </a>
     </div>
   );
